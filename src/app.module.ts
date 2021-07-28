221cdc61f54c194795caee8cbf6bc8636441bf65
a.osadchii <roflandcom@gmail.com>
2021-07-28 15:21:09 +0300

    mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import {TypegooseModule} from "nestjs-typegoose";
+import {getMongoConfig} from "./configs/mongo.config";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypegooseModule.forRootAsync({
+      imports: [],
+      inject: [ConfigService],
+      useFactory: getMongoConfig,
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
